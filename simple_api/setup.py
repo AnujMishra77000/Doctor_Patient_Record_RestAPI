54f772cd7cc682e6df68f6b568a7af85beb02b83
AnujMishra <anujm77000@gmail.com>
2024-08-29 11:53:29 +0530

    sdist and bdist file

--- simple_api/setup.py
+++ simple_api/setup.py
@@ -5,8 +5,8 @@ setup(
     author="Anuj Mishra",
     author_email="anujm77000@gmail.com",
     description="Making doctor and patient relation system how doctors and paitent can access there records using RestApi",
-    long_description=open('README.md').read(),
-    long_description_content_type="text/markdown",
+    #long_description=open('README.md').read(),
+    #long_description_content_type="text/markdown",
     url="https://github.com/AnujMishra77000/Hospitals_Doctor_Patient_record_system/tree/main/simple_api",
     packages=find_packages(),  # Automatically find packages in your project
     include_package_data=True,  # Include non-Python files specified in MANIFEST.in
